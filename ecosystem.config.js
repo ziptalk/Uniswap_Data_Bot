8c42873c474e2763de8d007415a428e6383617d3
Sonjeongbeom <sjb990221@gmail.com>
2023-05-18 17:29:53 +0900

    Fix: ecosystem.config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: 'binance-price-socket',
       script: './src/data/script/binance-price.js',
       instances: 1,
-      exec_mode: 'cluster',
+      exec_mode: 'fork',
       env: {
         MODE: 'BINANCE',
       },
@@ -17,12 +17,25 @@ module.exports = {
       name: 'uniswap-v3-socket',
       script: './src/data/script/pipeline-v3.js',
       instances: 1,
-      exec_mode: 'cluster',
+      exec_mode: 'fork',
       env: {
         MODE: 'SOCKET',
       },
       env_production: {
         MODE: 'SOCKET',
+      },
+      watch: false,
+    },
+    {
+      name: 'uniswap-v3-schedule',
+      script: './src/data/script/pipeline-v3.js',
+      instances: 1,
+      exec_mode: 'fork',
+      env: {
+        MODE: 'SCHEDULE',
+      },
+      env_production: {
+        MODE: 'SCHEDULE',
       },
       watch: false,
     },
@@ -30,7 +43,7 @@ module.exports = {
       name: 'uniswap-v2-socket',
       script: './src/data/script/pipeline-v2.js',
       instances: 1,
-      exec_mode: 'cluster',
+      exec_mode: 'fork',
       env: {
         MODE: 'SOCKET',
       },
@@ -40,23 +53,10 @@ module.exports = {
       watch: false,
     },
     {
-      name: 'uniswap-v3-scheduler',
-      script: './src/data/script/pipeline-v3.js',
-      instances: 1,
-      exec_mode: 'cluster',
-      env: {
-        MODE: 'SCHEDULE',
-      },
-      env_production: {
-        MODE: 'SCHEDULE',
-      },
-      watch: false,
-    },
-    {
-      name: 'uniswap-v2-scheduler',
+      name: 'uniswap-v2-schedule',
       script: './src/data/script/pipeline-v2.js',
       instances: 1,
-      exec_mode: 'cluster',
+      exec_mode: 'fork',
       env: {
         MODE: 'SCHEDULE',
       },
