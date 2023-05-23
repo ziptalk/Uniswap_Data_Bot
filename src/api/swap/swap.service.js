e3fefb268b6c03dc0df9f805bd33145013130469
Sonjeongbeom <sjb990221@gmail.com>
2023-05-23 17:49:17 +0900

    Add: get swap function

--- src/api/swap/swap.service.js
+++ src/api/swap/swap.service.js
@@ -1,6 +1,54 @@
+const { Database } = require('../../common/database');
+const { StringHandler } = require('../../common/string-handler');
+const { DateHandler } = require('../../common/date-handler');
+const { HttpException } = require('../http/http-exception');
+const { INTERVAL } = require('./interval');
+const dfd = require('danfojs-node');
+
 class SwapService {
   async getTest() {
     return 'test';
+  }
+
+  async getSwaps(symbol, interval) {
+    const table = StringHandler.makeValidTableName(symbol);
+    const selectQuery = `select * from ${table} order by timestamp;`;
+    let rows = await Database.execQuery(selectQuery);
+    if (!rows || !rows.length) {
+      throw new HttpException(400, `No data found (symbol : ${symbol})`);
+    }
+    for (let row of rows) {
+      const { timestamp } = row;
+      const newTimestamp = DateHandler.createMomentFromInterval(
+        timestamp,
+        INTERVAL[interval],
+      )
+        .utc()
+        .valueOf();
+      row.timestamp = newTimestamp;
+    }
+
+    let df = new dfd.DataFrame(rows);
+    const avgPrice = df['price'].mean({ axis: 0 });
+    df = df.fillNa([avgPrice], { columns: ['price'] });
+    df = df.iloc({ columns: [1, 3, 4] });
+    const quantityIn = df['quantity'].apply((val) => (val > 0 ? val : 0));
+    const quantityOut = df['quantity'].apply((val) => (val < 0 ? val : 0));
+    const quantityPrice = df.apply((row) => {
+      return row[1] * row[2];
+    });
+    df.addColumn('quantity_in', quantityIn['$data'], { inplace: true });
+    df.addColumn('quantity_out', quantityOut['$data'], { inplace: true });
+    df.addColumn('quantity_price', quantityPrice['$data'], { inplace: true });
+
+    const result = df.groupby(['timestamp']).agg({
+      quantity: 'sum',
+      price: 'mean',
+      quantity_in: 'sum',
+      quantity_out: 'sum',
+      quantity_price: 'sum',
+    });
+    return dfd.toJSON(result);
   }
 }
 
