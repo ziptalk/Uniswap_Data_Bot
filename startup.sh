dcaeccaf7962957094b9d97fbb08f6b2aec7db5f
Sonjeongbeom <sjb990221@gmail.com>
2023-06-14 18:16:01 +0900

    Fix: startup.sh .gitignore

--- startup.sh
+++ startup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-echo "Initial Script has been executed."
-pm2 start ecosystem.config.js --only uniswap-v3-socket >> startup.log 2>&1
-pm2 start ecosystem.config.js --only uniswap-v3-schedule >> startup.log 2>&1
+echo "Initial Script has been executed." >> startup.log 2>&1
+pm2 start ecosystem.config.js --only uniswap-v3-socket
+pm2 start ecosystem.config.js --only uniswap-v3-schedule
+pm2 list >> startup.log 2>&1
