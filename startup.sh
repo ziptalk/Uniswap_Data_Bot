7219366c1e8590238bb73c8c1cf6344bc0d4b8b7
Sonjeongbeom <sjb990221@gmail.com>
2023-06-14 18:26:04 +0900

    Fix: startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 echo "Initial Script has been executed." >> startup.log 2>&1
+sudo redis-server /etc/redis/redis.conf
 pm2 start ecosystem.config.js --only uniswap-v3-socket
 pm2 start ecosystem.config.js --only uniswap-v3-schedule
 pm2 list >> startup.log 2>&1
